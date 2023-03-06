8dd92022d54dd8c94b9b87cd8bf4030a51b36053
Zafer <zafereken@Zafer-MacBook-Pro.local>
2023-03-06 14:16:43 +0300

    webpack conf edited

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ const path = require('path');
 
 module.exports = {
   entry: './index.js',
-  mode: 'development',
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: 'index_bundle.js',
@@ -13,9 +12,7 @@ module.exports = {
     port: '3000',
     static: {
       directory: path.join(__dirname, 'public')
-},
-    open: true,
-    hot: true,
+    },
     liveReload: true,
   },
   resolve: {
